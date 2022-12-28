7c249e27fa8b9531fe01c078bac70e4d5290a494
acaldas <acaldas.dev@gmail.com>
2022-12-28 15:06:22 +0000

    Test deploy

--- src/Application.js
+++ src/Application.js
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Navigation, NavigationLink } from './Navigation';
-import { Page } from './Page';
+import { Navigation, NavigationLink } from "./Navigation";
+import { Page } from "./Page";
 
 function Application() {
   return (
     <main className="h-screen">
       <h1 className="text-3xl font-bold bg-yellow-300 p-4 border-b-2 border-yellow-400">
-        Incredibly Important Thoughts
+        Incredibly Important Second Thoughts
       </h1>
       <div className="grid grid-cols-12 grid-rows-1 gap-4 h-full">
         <Navigation>
